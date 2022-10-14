37b8ad1ea421e511c42bf64766202ee7a6ec4736
Ojore A. Chris <ojorechris@gmail.com>
2022-10-14 13:35:49 -0700

    betty was naggging

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
 
 /**
- *
- * main - prints the phrase "Programming is like building a multilingual puzzle
- * Return: 0 if exited properly, non-zero otherwise
- *
- *
+ * main - Function outputs "Programming is like building a multilingual puzzle"
+ * Return: 0 Function has no input
  */
 int main(void)
 {
-
 puts("\"Programming is like building a multilingual puzzle");
-
 return (0);
-
 }
