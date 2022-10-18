4191b9a21e7de0c24e4fae03ff019543c2342b8a
Ojore A. Chris <ojorechris@gmail.com>
2022-10-18 15:37:18 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - header file included
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void);
+{
+
+	char ch;
+
+	ch = 'a';
+
+while (ch <= 'z')
+
+{
+	_putchar(ch);
+	ch++;
+}
+	_putchar('\n');
+
+}
+
