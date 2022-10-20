6c71156454cf07add8e2599f0d7fd072d5be888c
Ojore A. Chris <ojorechris@gmail.com>
2022-10-20 11:29:09 -0700

    is upper modification

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,21 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
  *  _isupper(int c) - checks for uppercase character
- *  Return: 1 if c is uppercase
- *  Return: 0 if otherwise
+ *
+ *  Return: 1 if c is uppercase and 0 if otherwise
  */
 
 int _isupper(int c)
 {
 
-	return(c >= 'A' && c <= 'Z');
+if (c >= 'A' && c <= 'Z')
+{
+	return (1);
+
+} else {
+		return (0);
+	}
 
 }
