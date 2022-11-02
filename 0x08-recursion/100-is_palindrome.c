427d4f669ed7203c7ea7ec7b6e6a1fa830969cfe
Ojore A. Chris <ojorechris@gmail.com>
2022-11-02 09:45:08 -0700

    wild cmp

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -16,6 +16,12 @@ int is_palindrome(char *s)
 	return (check_pal(s, 0, _strlen_recursion(s)));
 }
 
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calc length
+ * Return: len of the string
+ */
+
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
