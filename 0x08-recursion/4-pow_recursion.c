e4cd874c5ec6953738127bb56df4a423f66e70c8
Ojore A. Chris <ojorechris@gmail.com>
2022-11-02 08:30:37 -0700

    updates to power recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * that returns the value of x raised to the power of y
+ * _pow_recursion - returns the value of x raised to the power of y
  * @x: int value of base
  * @y: int value of y
  * Return: int result
