c6f57fce066a3b7c9be62912e08e37de6c34104b
Ojore A. Chris <ojorechris@gmail.com>
2022-11-07 12:19:14 -0800

    var was added to hold value

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,6 +13,7 @@ int main(int argc, char *argv[])
 {
 	int num1 = atoi(argv[1]);
 	int num2 = atoi(argv[2]);
+	int result;
 
 	if (argc != 3)
 	{
@@ -20,7 +21,9 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	printf("%d\n", num1 * num2);
+	result = num1 * num2;
+
+	printf("%d\n", result);
 
 	return (0);
 
