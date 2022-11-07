35cf094b8ecd1baf6d6398fb14a2020fed61d6f7
Ojore A. Chris <ojorechris@gmail.com>
2022-11-07 13:31:11 -0800

    segmentation error corrected

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
 
 /**
@@ -10,15 +11,16 @@
 
 int main(int argc, char *argv[])
 {
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-	int result;
+	int num1, num2, result;
 
-	if (argc != 3)
+	if (argc < 3 || argc> 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
 
 	result = num1 * num2;
 
