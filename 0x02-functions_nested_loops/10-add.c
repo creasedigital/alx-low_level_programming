36fddee9a85dbe8046c91b0bc89bf0c6279eecd4
Ojore A. Chris <ojorechris@gmail.com>
2022-11-09 02:53:50 -0800

    addition formula

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: first int
+ * @b: second int
+ * Return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
