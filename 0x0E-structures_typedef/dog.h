e7ac8f0156fb848324fe1d1ee378fba67ef71c54
Ojore A. Chris <ojorechris@gmail.com>
2022-11-14 06:29:59 -0800

    dog struct header

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - dog profile
+ * @name: First input
+ * @age: second input
+ * @owner: third input
+ * Description: a struct construct for a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
