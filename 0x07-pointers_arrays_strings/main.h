e1f1d1566c0a5f9f5d0509ec1ba6d748b9501c6e
Ojore A. Chris <ojorechris@gmail.com>
2022-11-25 04:22:01 -0800

    header added to prototype

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,5 +9,6 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_diagsums(int *a, int size);
 int _putchar(char c);
+void set_string(char **s, char *to);
 
 #endif
