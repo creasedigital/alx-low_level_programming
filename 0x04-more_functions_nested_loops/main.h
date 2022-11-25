ddcabe35c0fccb57248fd2934299ab16ce263518
Ojore A. Chris <ojorechris@gmail.com>
2022-11-25 04:13:30 -0800

    putchar added

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,4 @@
 #ifndef HEADER_FILE
-
 #define HEADER_FILE
 
 int _isupper(int c);
@@ -13,5 +12,6 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+int _putchar(char c);
 
 #endif
