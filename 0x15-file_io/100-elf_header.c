42b8623274d5192b5b18393afad78c66a212907a
Ojore A. Chris <ojorechris@gmail.com>
2022-12-09 11:31:29 -0800

    elf header

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - displays the information contained in the ELF header
+ * @argc: number of arguments passed to the program
+ * @argv: array of arguments
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+
+
+	return (0);
+}
