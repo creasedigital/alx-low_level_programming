f7fd25452a98e3e81a9d77428d9b633417790ff7
Ojore A. Chris <ojorechris@gmail.com>
2023-01-16 12:51:30 -0800

    files

--- 0x18-dynamic_libraries/1-isdigit.c
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -1,19 +1,21 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _isdigit - checks for a digit (0 through 9)
- * @c: int to be checked
- * Return: 1 if c is a digit, 0 otherwise
+ *  _isupper - checks for uppercase character
+ * @c: variable test
+ *  Return: 1 if c is uppercase and 0 if otherwise
  */
 
-int _isdigit(int c)
+int _isupper(int c)
 {
 
-	if (c >= '0' && c <= '9')
-		return (1);
+if ((c >= 'A') && (c <= 'Z'))
 
-	else
-		return (0);
+	return (1);
+
+else
+	return (0);
+
+
 
 }
--- 0x18-dynamic_libraries/_putchar.c
+++ 0x18-dynamic_libraries/_putchar.c
@@ -1,10 +1,9 @@
-#include "main.h"
 #include <unistd.h>
 
 /**
- * _putchar - writes to stdout
- * @c: char input
- * Return: always 0 (success)
+ * _putchar - writes a char to stdout
+ * @c: the char to print
+ * Return: 1 on success err -1 and err no set appropriately
  */
 
 int _putchar(char c)
