a8fb31fc5afb3437edd86dce091d31eb00fd022f
Ojore A. Chris <ojorechris@gmail.com>
2023-01-16 13:11:16 -0800

    soft

--- 0x18-dynamic_libraries/1-isdigit.c
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -1,21 +1,19 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- *  _isupper - checks for uppercase character
- * @c: variable test
- *  Return: 1 if c is uppercase and 0 if otherwise
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: int to be checked
+ * Return: 1 if c is a digit, 0 otherwise
  */
 
-int _isupper(int c)
+int _isdigit(int c)
 {
 
-if ((c >= 'A') && (c <= 'Z'))
+	if (c >= '0' && c <= '9')
+		return (1);
 
-	return (1);
-
-else
-	return (0);
-
-
+	else
+		return (0);
 
 }
