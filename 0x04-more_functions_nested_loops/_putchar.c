c68b43e322b8f364b2967824ba7fdbeca87f941e
Ojore A. Chris <ojorechris@gmail.com>
2023-01-17 00:45:35 -0800

    nested func digit

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,15 +1,12 @@
 #include <unistd.h>
-#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: the character to print
- * Return: on success 1 else -1
+ * _putchar - writes a char to stdout
+ * @c: the char to print
+ * Return: 1 on success err -1 and err no set appropriately
  */
 
 int _putchar(char c)
 {
-
 	return (write(1, &c, 1));
-
 }
